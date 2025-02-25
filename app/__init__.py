0b46ade6a28af0390a504b06aa2591dc30cfb717
= <alucasvalle@gmail.com>
2025-02-25 01:48:26 -0300

    chore: import static and templates from previous project (biblion frontend)

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 from config import Config
 
 def create_app():
@@ -6,8 +6,51 @@ def create_app():
     app = Flask(__name__, static_folder='static', template_folder='templates')
     app.config.from_object(Config)
 
-    @app.route("/")
+    @app.route('/')
+    def intro():
+        return render_template('index.html')
+
+    @app.route('/login',  methods=['GET', 'POST'])
+    def login():
+        if request.method == 'POST':
+            email = request.form['email']
+            password = request.form['password']
+            response = {
+                "email": email, 
+                "password": password
+            }
+
+            return jsonify(response)
+
+        return render_template('login.html')
+
+    @app.route('/register')
+    def register():
+        return render_template('register.html')
+
+    @app.route('/forgot-password')
+    def forgot_password():
+        return render_template('forgot-password.html')
+
+    @app.route('/home')
     def home():
-        return "Flask is running!"
+        return render_template('home.html')
+
+    @app.route('/book-details')
+    def book_details():
+        return render_template('book-details.html')
+
+    @app.route('/reading')
+    def reading():
+        return render_template('reading.html')
+
+    @app.route('/favorites')
+    def favorites():
+        return render_template('favorites.html')
+
+    @app.route('/profile')
+    def profile():
+        return render_template('profile.html')
+
 
     return app
