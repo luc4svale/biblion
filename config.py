b7536e7abb71235ececdff60552e830ba188107d
= <alucasvalle@gmail.com>
2025-02-25 05:30:54 -0300

    chore: update configuration for SQLAlchemy database URI

--- config.py
+++ config.py
@@ -1,13 +1,21 @@
 import os
 from dotenv import load_dotenv
 
-# Load .env variables
 load_dotenv()
 
-class Config: # pylint: disable=too-few-public-methods
+class Config:
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    INSTANCE_DIR = os.path.join(BASE_DIR, "instance")
+
+    if not os.path.exists(INSTANCE_DIR):
+        os.makedirs(INSTANCE_DIR)
+
     SECRET_KEY = os.getenv('SECRET_KEY')
     if not SECRET_KEY:
-        raise ValueError("SECRET_KEY is not defined! Verify the environment variables")
+        raise ValueError("SECRET_KEY is not defined! Verify the environment variables.")
 
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
+    DATABASE_URL = os.getenv('DATABASE_URL',
+                             f"sqlite:///{os.path.join(INSTANCE_DIR, 'db.sqlite3')}")
+
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
