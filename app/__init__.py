018eb3dafc557f3aad19ca8675e528974636888f
= <alucasvalle@gmail.com>
2025-02-25 06:12:49 -0300

    refactor: move route definitions to separate routes.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,56 +1,17 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask
+from flask_migrate import Migrate
 from config import Config
+from .models.database import db
+from .routes import init_routes
 
 def create_app():
     """Factory function to create and configure the Flask application"""
     app = Flask(__name__, static_folder='static', template_folder='templates')
     app.config.from_object(Config)
 
-    @app.route('/')
-    def intro():
-        return render_template('index.html')
+    db.init_app(app)
+    Migrate(app, db)
 
-    @app.route('/login',  methods=['GET', 'POST'])
-    def login():
-        if request.method == 'POST':
-            email = request.form['email']
-            password = request.form['password']
-            response = {
-                "email": email, 
-                "password": password
-            }
-
-            return jsonify(response)
-
-        return render_template('login.html')
-
-    @app.route('/register')
-    def register():
-        return render_template('register.html')
-
-    @app.route('/forgot-password')
-    def forgot_password():
-        return render_template('forgot-password.html')
-
-    @app.route('/home')
-    def home():
-        return render_template('home.html')
-
-    @app.route('/book-details')
-    def book_details():
-        return render_template('book-details.html')
-
-    @app.route('/reading')
-    def reading():
-        return render_template('reading.html')
-
-    @app.route('/favorites')
-    def favorites():
-        return render_template('favorites.html')
-
-    @app.route('/profile')
-    def profile():
-        return render_template('profile.html')
-
+    init_routes(app)
 
     return app
---
+++ app/routes.py
@@ -0,0 +1,49 @@
+from flask import render_template, request, jsonify
+
+def init_routes(app):
+    """Função para registrar as rotas na aplicação"""
+
+    @app.route('/')
+    def intro():
+        return render_template('index.html')
+
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        if request.method == 'POST':
+            email = request.form['email']
+            password = request.form['password']
+            response = {
+                "email": email, 
+                "password": password
+            }
+            return jsonify(response)
+
+        return render_template('login.html')
+
+    @app.route('/register')
+    def register():
+        return render_template('register.html')
+
+    @app.route('/forgot-password')
+    def forgot_password():
+        return render_template('forgot-password.html')
+
+    @app.route('/home')
+    def home():
+        return render_template('home.html')
+
+    @app.route('/book-details')
+    def book_details():
+        return render_template('book-details.html')
+
+    @app.route('/reading')
+    def reading():
+        return render_template('reading.html')
+
+    @app.route('/favorites')
+    def favorites():
+        return render_template('favorites.html')
+
+    @app.route('/profile')
+    def profile():
+        return render_template('profile.html')
