55c7116dfac573681f543782e3a21dda543f272d
Lucas Vale <alucasvalle@gmail.com>
2025-02-28 20:05:51 -0300

    add login_required on private routes and update requirements

--- app/routes/books.py
+++ app/routes/books.py
@@ -1,19 +1,28 @@
 from flask import Blueprint, render_template
+from flask_login import login_required
 
 books_bp = Blueprint("books", __name__)
 
+
 @books_bp.route("/home", methods=["GET"])
+@login_required
 def home():
     return render_template("private/home.html")
 
+
 @books_bp.route("/book-details", methods=["GET"])
+@login_required
 def book_details():
     return render_template("private/book-details.html")
 
+
 @books_bp.route("/favorites", methods=["GET"])
+@login_required
 def favorites():
     return render_template("private/favorites.html")
 
+
 @books_bp.route("/reading", methods=["GET"])
+@login_required
 def reading():
     return render_template("private/reading.html")
--- config.py
+++ config.py
@@ -2,10 +2,6 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
-print(os.getenv('DATABASE_URL'))
-print(os.getenv('SECRET_KEY'))
-
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///default.db')
