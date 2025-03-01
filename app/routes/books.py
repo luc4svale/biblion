641f493b731d2edb7550e2f36f064f5af5630e95
Andre-Luis-Aguiar <andreluisaguiar2000@gmail.com>
2025-02-28 23:10:03 -0300

    adiciona rota para cadastrar livros

--- app/routes/books.py
+++ app/routes/books.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
+from app.models import Book
 
 books_bp = Blueprint("books", __name__)
 
@@ -7,13 +8,14 @@ books_bp = Blueprint("books", __name__)
 @books_bp.route("/home", methods=["GET"])
 @login_required
 def home():
-    return render_template("private/home.html")
-
+    books = Book.query.all() 
+    return render_template("private/home.html", books=books)
 
 @books_bp.route("/book-details", methods=["GET"])
 @login_required
-def book_details():
-    return render_template("private/book-details.html")
+def book_details(book_id):
+    book = Book.query.get_or_404(book_id)
+    return render_template("private/book-details.html", book=book)
 
 
 @books_bp.route("/favorites", methods=["GET"])
@@ -26,3 +28,9 @@ def favorites():
 @login_required
 def reading():
     return render_template("private/reading.html")
+
+@books_bp.route("/book-register", methods=["GET"])
+@login_required
+def book_register():
+    return render_template("private/book-register.html")
+
